19626ebba29c9505cf6bab16cb28b09d928f8c8c
fnhe <116603529+fnhe@users.noreply.github.com>
2023-02-07 13:26:33 -0600

    Update 2_Gene_Expression.py

--- pages/2_Gene_Expression.py
+++ pages/2_Gene_Expression.py
@@ -13,7 +13,7 @@ cancer_ranking_all = [ 'Osteosarcoma', 'Clear Cell Sarcoma', 'Other Sarcoma',   
 color_ranking_palette = [ f(.6), f(.8), f(.3), h(.6), b(.6), a(.6), h(.2), e(.6)]
 color_ranking_dict = {'Osteosarcoma':  f(.6),  'Clear Cell Sarcoma':f(.8), 'Other Sarcoma':f(.3),  'Wilms Tumor': h(.6), 'Hepatoblastoma':b(.6),
                      'Germ Cell Tumor': a(.6),   'Neuroblastoma':h(.2), 'Other Tumor': e(.6)}
-def plot_box_with_ax_datatype(dt, X, Y, ax):
+def plot_box_with_ax_datatype1(dt, X, Y, ax):
     cancer_ranking1 = dt.groupby(X).median().sort_values(Y, ascending=False).index
     color_ranking1 = [color_ranking_palette[cancer_ranking_all.index(i)]  for i in cancer_ranking1 ]
 
@@ -29,6 +29,28 @@ def plot_box_with_ax_datatype(dt, X, Y, ax):
     p = sns.stripplot(data = dt[dt['Datatype'] == 'PT'], x = X, y = Y,  ax =ax, jitter=0.1,
               color='steelblue', marker = 'o', size = 9, alpha = 0.9,  linewidth=0.5, edgecolor = '.2',
              order =cancer_ranking1)
+    p = sns.stripplot(data = dt[dt['Datatype'] == 'PDX'],  x = X, y = Y,  ax = ax,jitter=0.1,
+              color='orange', marker = '^', size = 9, alpha = 0.9, linewidth=0.5, edgecolor = '.2',
+             order =cancer_ranking1)
+    
+    p.set_xticklabels(cancer_ranking1,  ha='right', rotation_mode='anchor', rotation = 45)
+    p.set_xlabel('')
+    sns.despine()
+    p.legend(bbox_to_anchor = (1,1), frameon=False)
+    
+def plot_box_with_ax_datatype2(dt, X, Y, ax):
+    cancer_ranking1 = dt.groupby(X).median().sort_values(Y, ascending=False).index
+    color_ranking1 = [color_ranking_palette[cancer_ranking_all.index(i)]  for i in cancer_ranking1 ]
+
+
+    p = sns.boxplot(data = dt, x = X, y = Y,  order = cancer_ranking1,  palette=color_ranking1, ax = ax,
+            fliersize=0,
+                boxprops={'edgecolor': 'black', 'alpha':1,'linewidth':0, },  
+                medianprops={'color':'black', 'alpha':1, 'linewidth':1.5},
+                whiskerprops={'linewidth':1, 'color':'black','alpha' : 1},
+           )
+    plt.setp(p.collections, alpha=.7, linewidth=0)
+
     p = sns.stripplot(data = dt[dt['Datatype'] == 'PDX'],  x = X, y = Y,  ax = ax,jitter=0.1,
               color='orange', marker = '^', size = 9, alpha = 0.9, linewidth=0.5, edgecolor = '.2',
              order =cancer_ranking1)
@@ -67,9 +89,9 @@ ax2 = plt.subplot(gs[0, 1])
 
 X = 'Cancer'
 Y = gene_sel
-g1 = plot_box_with_ax_datatype(info2plot, X, Y, ax1)
+g1 = plot_box_with_ax_datatype1(info2plot, X, Y, ax1)
 ax1.set_title('PT and PDX samples')
-g2 = plot_box_with_ax_datatype(info2plot[info2plot['Datatype'] == 'PDX'], X, Y, ax2)
+g2 = plot_box_with_ax_datatype2(info2plot[info2plot['Datatype'] == 'PDX'], X, Y, ax2)
 ax2.set_title('PDX samples')
 
 ylim = st.slider(
