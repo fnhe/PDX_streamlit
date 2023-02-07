c325066e83fa8e8cbb919ce3ef18673a2a16fb41
fnhe <116603529+fnhe@users.noreply.github.com>
2023-02-07 17:55:28 -0600

    Update 4_Request.py

--- pages/4_Request.py
+++ pages/4_Request.py
@@ -20,21 +20,21 @@ with st.form('PDX data request'):
     with col2:
         ins = st.text_input("Institution")
     with col3:
-		country = st.text_input("Country")
+        country = st.text_input("Country")
     with col1:
         pos = st.selectbox("Your position", ['', 'Researcher','Professor','Student','Other'])
 
 
     col1, col2 = st.columns(2)
-	with col1:
-		email = st.text_input("Email") 
-	with col2:
-		phone = st.text_input("Phone number")
-	other = st.text_area("Other")
-	submit_info = st.form_submit_button(label = 'Submit', on_click=print,)
+    with col1:
+        email = st.text_input("Email") 
+    with col2:
+        phone = st.text_input("Phone number")
+    other = st.text_area("Other")
+    submit_info = st.form_submit_button(label = 'Submit', on_click=print,)
 
-	if submit_info:
-		if email and ids and ins and fn:
+    if submit_info:
+        if email and ids and ins and fn:
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
             server.login(sender, 'vvljquqzjrqleduy')
@@ -42,7 +42,5 @@ with st.form('PDX data request'):
             
             message = submit_info
             server.sendmail(sender, receiver, message)
-		else:
-			st.warning('Please fill PDX IDs, Email, Institute and Name')
-
-#st.form
+        else:
+            st.warning('Please fill PDX IDs, Email, Institute and Name')
