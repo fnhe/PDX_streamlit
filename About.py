0bf087a968e09d4e3a3a40d2f6ef6bfbdeaf5de5
fnhe <116603529+fnhe@users.noreply.github.com>
2023-02-14 14:24:36 -0600

    Update About.py

--- About.py
+++ About.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-st.set_page_config(page_icon="🧊",
+st.set_page_config(#page_icon="🧊",
     layout="centered",
     initial_sidebar_state="expanded")
 st.title('Pediatric Solid Tumor PDX Portal')
