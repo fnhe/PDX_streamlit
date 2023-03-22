0bb76806775b1995d1dba87b6aa5605ad2f04d44
fnhe <116603529+fnhe@users.noreply.github.com>
2023-03-22 16:17:28 -0500

    Update About.py

--- About.py
+++ About.py
@@ -8,11 +8,11 @@ import seaborn as sns
 st.set_page_config(#page_icon="🧊",
     layout="centered",
     initial_sidebar_state="expanded",
-    page_title="pstPDX"
+    page_title="Pediatric Solid Tumor PDX"
     )
-st.title('Pediatric Solid Tumor PDX Portal (pstPDX)')
+st.header('Pediatric Solid Tumor PDX Portal (pstPDX)')
 
-st.write('Patient-Derived Xenografts (PDX) are an important tool for developing new therapies and understanding cancer biology in pediatric oncology. We have developed 68 PDXs from rare solid childhood cancers in a joint effort between Greehey Children’s Cancer Research Institute UT Health San Antonio and UT Southwestern. These models have been profiled with one or more sequencing methods. ')
+st.text('Patient-Derived Xenografts (PDX) are an important tool for developing new therapies and understanding cancer biology in pediatric oncology. We have developed 68 PDXs from rare solid childhood cancers in a joint effort between Greehey Children’s Cancer Research Institute UT Health San Antonio and UT Southwestern. These models have been profiled with one or more sequencing methods. ')
 
 st.write('We have created :blue[pstPDX] portal to share this resource with the community. On the portal users can query and request PDXs. The gene expression page also allows users to compare the expression of a gene across cancer types. ')
 
